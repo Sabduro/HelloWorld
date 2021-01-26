4eb57847577f3c6c21553b290a55d7b621f8dbca
Sabduro <78004788+Sabduro@users.noreply.github.com>
2021-01-25 22:46:38 -0500

    Initial commit

---
+++ src/HellowWorld.java
@@ -0,0 +1,16 @@
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("i: " + i);
+//			System.out.println("Hellooooo World!");
+//		}
+		for (int i = 0; i < 10; i++) {
+			if ((i % 2) == 0) {
+				System.out.println("i: " + i);
+				System.out.println("Hellooooo World!");
+			}
+		}
+	}
+}
